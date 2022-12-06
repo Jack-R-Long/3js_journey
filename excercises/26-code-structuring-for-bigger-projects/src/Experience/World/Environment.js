fc6f329ffaea0ce1c0d808aed3a3f29d3a9e96e6
Jack Long <jack.richard.long@gmail.com>
2022-12-06 15:10:34 -0600

    26 & 27

---
+++ excercises/26-code-structuring-for-bigger-projects/src/Experience/World/Environment.js
@@ -0,0 +1,72 @@
+import Experience from "../Experience";
+import * as THREE from "three";
+
+export default class Environment {
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
+    this.debug = this.experience.debug;
+
+    this.setSunLight();
+    this.setEnvironmentMap();
+  }
+
+  setSunLight() {
+    this.sunLight = new THREE.DirectionalLight("#ffffff", 3);
+    this.sunLight.castShadow = true;
+    this.sunLight.shadow.camera.far = 15;
+    this.sunLight.shadow.mapSize.set(1024, 1024);
+    this.sunLight.shadow.normalBias = 0.05;
+    this.sunLight.position.set(3.5, 2, -1.25);
+
+    this.scene.add(this.sunLight);
+
+    // Debug
+    if (this.debug.active) {
+      this.debugFolder = this.debug.gui.addFolder("sun");
+      this.debugFolder.add(this.sunLight.position, "x").min(-5).max(5).step(0.001);
+      this.debugFolder.add(this.sunLight.position, "y").min(-5).max(5).step(0.001);
+      this.debugFolder.add(this.sunLight.position, "z").min(-5).max(5).step(0.001);
+      this.debugFolder.add(this.sunLight, "intensity").min(0).max(10).step(0.001);
+    }
+  }
+
+  setEnvironmentMap() {
+    this.environmentMap = {};
+    this.environmentMap.intensity = 0.4;
+    this.environmentMap.texture = this.resources.items.environmentMapTexture;
+    this.environmentMap.texture.encoding = THREE.sRGBEncoding;
+
+    this.scene.environment = this.environmentMap.texture;
+
+    this.environmentMap.updateMaterials = () => {
+      // Yes we can put a methods inside properites of an object
+      this.scene.traverse((child) => {
+        if (
+          child instanceof THREE.Mesh &&
+          child.material instanceof THREE.MeshStandardMaterial
+        ) {
+          child.material.envMap = this.environmentMap.texture;
+          child.material.envMapIntensity = this.environmentMap.intensity;
+          child.material.needsUpdate = true;
+        }
+      });
+    };
+    this.environmentMap.updateMaterials();
+
+    // Debug
+    if (this.debug.active) {
+      this.debugFolder = this.debug.gui.addFolder("environment");
+      this.debugFolder
+        .add(this.environmentMap, "intensity")
+        .min(0)
+        .max(1)
+        .step(0.001)
+        .onChange(() => {
+          this.environmentMap.updateMaterials();
+        });
+    }
+  }
+
+}
---
+++ excercises/26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -0,0 +1,31 @@
+import Experience from "../Experience";
+import Environment from "./Environment";
+import Floor from "./Floor";
+import Fox from "./Fox";
+
+export default class World {
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
+
+
+    // Listed for resources ready
+    this.resources.on("ready", () => {
+      // Setup
+      this.floor = new Floor();
+      this.fox = new Fox();
+      this.environment = new Environment();
+    });
+
+    this.update();
+  }
+
+  update() 
+  {
+    if (this.fox)
+    {
+      this.fox.update();
+    }
+  }
+}
